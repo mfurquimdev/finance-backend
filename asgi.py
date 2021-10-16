d9e6ebe6a03a28d27ba418317d2a5ea892586c5e
Mateus Furquim <furquim.axs@gmail.com>
2021-10-16 18:14:31 -0300

    Configure a better prometheus instrumentor

--- asgi.py
+++ asgi.py
@@ -6,6 +6,18 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 log.info('get_app()')
 app = get_app()
+
+instrumentator = Instrumentator(
+    should_group_status_codes=True,
+    should_ignore_untemplated=True,
+    should_respect_env_var=True,
+    should_instrument_requests_inprogress=True,
+    excluded_handlers=[".*admin.*", "/metrics"],
+    env_var_name="ENABLE_METRICS",
+    inprogress_name="inprogress",
+    inprogress_labels=True,
+)
+
 Instrumentator().instrument(app).expose(app)
 
 if __name__ == '__main__':
