a038ded430900ac25ad069bbcbd83ce2e377a8ec
Mateus Furquim <furquim.axs@gmail.com>
2021-10-16 17:53:25 -0300

    Instrument code, expose metrics for prometheus

--- asgi.py
+++ asgi.py
@@ -1,8 +1,12 @@
 from app.main import get_app
 from app.log_manager import log
 
+from prometheus_fastapi_instrumentator import Instrumentator
+
+
 log.info('get_app()')
 app = get_app()
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == '__main__':
     import uvicorn
